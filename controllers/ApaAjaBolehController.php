44d658d3d952a1dbb902cb0df40068915dd9ef89
ibnuda <ibnu.d.a@student.uns.ac.id>
2016-03-10 10:44:39 +0700

    saya pilek.

---
+++ controllers/ApaAjaBolehController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: IT Support
+ * Date: 2016-03-10
+ * Time: 10:38 AM
+ */
+
+namespace app\controllers;
+
+use Yii;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+
+class ApaAjaBolehController extends Controller
+{
+}
